04269e99ac1e3a8e1be29d789e78551bba5e7a0a
Jason Few <omence11@gmail.com>
2019-06-09 16:11:50 -0700

    Home page started

---
+++ CCMS/CCMS/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PhaseOne()
+        {
+            return View();
+        }
+
+        public IActionResult PhaseTwo()
+        {
+            return View();
+        }
+    }
+}
